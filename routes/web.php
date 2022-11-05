ebdaae9f12ebc5a57d2135690652b3015cc344de
test user <test@example.com>
2022-11-04 21:33:17 -0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ |--------------------------------------------------------------------------
+ | Web Routes
+ |--------------------------------------------------------------------------
+ |
+ | Here is where you can register web routes for your application. These
+ | routes are loaded by the RouteServiceProvider within a group which
+ | contains the "web" middleware group. Now create something great!
+ |
+ */
 
-		Route::get('/', function () { 
-					return 'главная страница сайта'; 
-		}); 
-		Route::get('/posts/', function () { 
-			return 'список постов'; }); 						Route::get('/post/1/', function () { 
-			return 'один пост'; }); ?>
+Route::get(
+	'/', function () {
+	    return 'главная страница сайта';
+    }
+);
+Route::get(
+	'/posts/', function () {
+	    return 'список постов';
+    }
+);
+Route::get(
+	'/post/1/', function () {
+	    return 'один пост';
+    }
+); 
 
