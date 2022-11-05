49215374749ef173a55e55bec1cd896fed74455f
test user <test@example.com>
2022-11-04 21:05:52 -0700

    Initial reposytory

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+<?php 
+		Route::get('/', function () { 
+					return 'главная страница сайта'; 
+		}); 
+		Route::get('/posts/', function () { 
+			return 'список постов'; }); 						Route::get('/post/1/', function () { 
+			return 'один пост'; }); ?>
+
